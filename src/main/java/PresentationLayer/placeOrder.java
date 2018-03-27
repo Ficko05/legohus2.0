e933a68d004d8d1a09b8c2e5b395d7ddd06b12fc
Ficko <Ficko@Ficko-Pc.lan>
2018-03-27 12:33:31 +0200

    legohus 1

--- src/main/java/PresentationLayer/placeOrder.java
+++ src/main/java/PresentationLayer/placeOrder.java
@@ -10,7 +10,7 @@ public class placeOrder extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
 
-        
+        System.out.println("gg");
         
         return "customerOrder";
 
